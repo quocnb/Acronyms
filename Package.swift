f866689954896ad95212eb8e949935df90a83ba6
Quoc Nguyen <quocnb.vnu@gmail.com>
2018-09-03 14:07:18 +0900

    Create website with leaf

--- Package.swift
+++ Package.swift
@@ -1,17 +1,16 @@
 // swift-tools-version:4.0
 import PackageDescription
-
 let package = Package(
     name: "TILApp",
     dependencies: [
-        // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-        .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0"),
-    ],
+        .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0-rc"),
+        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc")
+        ],
     targets: [
-        .target(name: "App", dependencies: ["FluentPostgreSQL", "Vapor"]),
+        .target(name: "App",
+        dependencies: ["FluentPostgreSQL", "Vapor", "Leaf"]),
         .target(name: "Run", dependencies: ["App"]),
-        .testTarget(name: "AppTests", dependencies: ["App"])
-    ]
+        .testTarget(name: "AppTests", dependencies: ["App"]),
+        ]
 )
-
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,11 +1,12 @@
 import FluentPostgreSQL
 import Vapor
+import Leaf
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
     /// Register providers first
     try services.register(FluentPostgreSQLProvider())
-
+    try services.register(LeafProvider())
     /// Register routes to the router
     let router = EngineRouter.default()
     try routes(router)
@@ -51,4 +52,5 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     commandConfig.useFluentCommands()
     services.register(commandConfig)
 
+    config.prefer(LeafRenderer.self, for: ViewRenderer.self)
 }
